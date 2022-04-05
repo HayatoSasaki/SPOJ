da69ffc2b6ae772d7b2e0efbe5aa0aba3e2b337d
Gabe <99471382+HayatoSasaki@users.noreply.github.com>
2022-04-04 23:08:54 -0300

    TLE 3.0
    Better for big numbers?

--- ID002.py
+++ ID002.py
@@ -2,14 +2,24 @@
 
 t = int(input()) # Número de testes.
 
+def prime(p):
+    i = 5 # Checker.
+
+    if p == 1 : # Skippa o 1.
+        return False
+    if p == 2 or p == 3: # Prima o 2 e 3.
+        return True
+    if p % 2 == 0 or p % 3 == 0 : # Skippa divisíveis deles.
+        return False
+    while i*i <= p:
+        if p % i == 0 or p % (i+2) == 0:
+            return False
+        i += 6 
+    return True
+
 for x in range(t): # Realiza 't' testes.
     m, n = [int(m) for m in input().split()] # Recebe o intervalo.
 
     for m in range(m,n+1): # Checka o intervalo.
-        if m > 1:
-                for i in range(2,m+1): # Checka divisores individualmente.
-                    if m == i:
-                        print(m)
-                        break
-                    elif m % i == 0:
-                        break
+        if prime(m): # Se retornar True, printa.
+            print(m)
