09c43bba123f0c16615122a90ed4ff1febcaf07a
Gabe <99471382+HayatoSasaki@users.noreply.github.com>
2022-04-04 21:30:20 -0300

    TLE
    ...

--- ID002.py
+++ ID002.py
@@ -2,18 +2,14 @@
 
 t = int(input()) # Número de testes.
 
-def prime(m):
-    for i in range(2,m):
-        if m % i == 0:
-            return
-    print(m)
-    return
+for x in range(t): # Realiza 't' testes.
+    m, n = [int(m) for m in input().split()] # Recebe o intervalo.
 
-for x in range(t):
-    m, n = input().split() # Recebe o intervalo.
-    m = int(m) # Converte.
-    n = int(n) # Converte.
-          
     for m in range(m,n+1): # Checka o intervalo.
         if m > 1:
-            prime(m)
+                for i in range(2,m+1): # Checka divisores individualmente.
+                    if m == i:
+                        print(m)
+                        break
+                    elif m % i == 0:
+                        break
