5be30e0a167a823be1585ef881addc6c56f117af
Gabe <99471382+HayatoSasaki@users.noreply.github.com>
2022-04-07 01:42:04 -0300

    SIGKILL
    ITS FUCKING 1 BILLION NUMBERS, isPrime can't do this. Saving just because.

--- ID002.py
+++ ID002.py
@@ -1,31 +1,30 @@
 # PRIME1 - Prime Generator | https://www.spoj.com/problems/PRIME1/
+# ITS FUCKING 1 BILLION NUMBERS, isPrime can't do this. Saving just because.
 
-global primeList
+def isPrime(n2: int):
+    isPrime = [True] * n2 
+    isPrime[0] = False 
+    isPrime[1] = False 
+    for i in range(2, n2): # Save the primes in primeList.
+        if isPrime[i]:
+            primeList.append(i)
+            for j in range(i*i, n2, i): # Set the multiples False.
+                isPrime[j] = False
 
-def sieve(n1: int):
-    n1 += 1
-    if n1 >= 2: # Se n == 1 nem se dá o trabalho.
-        isPrime = [True] * n1 # Intervalo [0,n].
-        isPrime[0] = False # Skippa 0.
-        isPrime[1] = False # Skippa 1.
-        for i in range(2, n1): # Marca os primes.
-            if isPrime[i]:
-                primeList.append(i)
-                for j in range(i*i, n1, i): # Elimina os multiplos.
-                    isPrime[j] = False
-    return
-
-def output(m2,n2: int):
-    sieve(n2) # Guarda os primes até n via Simple Sieve.
-    p = 0
-    while m2 > primeList[p]: # Acha o começo.
-        p += 1
-    for pp in range(p,len(primeList)): # Printa os primes até n.
-        print(primeList[pp])
+def output(m1,n1: int):
+    if m1 == 1:
+        m1 += 1
+    isPrime(n1+1) # Fill the list of primes.
+    for prime in range(len(primeList)): # Find the beginning.
+        if primeList[prime] >= m1:
+            break
+    for prime in range(prime, len(primeList)): # Print the primes between m and n.
+        print(primeList[prime])
 
 # ------- Main.
-t = int(input()) # Número de testes.
-for x in range(t): # Realiza 't' testes.
-    m, n = [int(i) for i in input().split()] # Recebe o intervalo.
-    primeList = [] 
+global primeList
+t = int(input()) # Number of testes.
+for x in range(t): # Performs 't' tests.
+    primeList = [] # Create/Reset the list of primes.
+    m, n = [int(k) for k in input().split()] # Reads the range.
     output(m,n)
