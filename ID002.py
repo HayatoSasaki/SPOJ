1957fcc90e62871a93f2b48e41c95e2f78cdcf4a
Gabe <99471382+HayatoSasaki@users.noreply.github.com>
2022-04-08 21:25:06 -0300

    Learning Seg Sieve.

--- ID002.py
+++ ID002.py
@@ -1,30 +1,31 @@
 # PRIME1 - Prime Generator | https://www.spoj.com/problems/PRIME1/
 # ITS FUCKING 1 BILLION NUMBERS, isPrime can't do this. Saving just because.
 
-def isPrime(n2: int):
+def isPrime(n2: int): # Define Simple Sieve.
+    lp2 = [] # Create the list of primes.
     isPrime = [True] * n2 
     isPrime[0] = False 
     isPrime[1] = False 
     for i in range(2, n2): # Save the primes in primeList.
         if isPrime[i]:
-            primeList.append(i)
+            lp2.append(i)
             for j in range(i*i, n2, i): # Set the multiples False.
                 isPrime[j] = False
+    return lp2
 
-def output(m1,n1: int):
-    if m1 == 1:
-        m1 += 1
-    isPrime(n1+1) # Fill the list of primes.
-    for prime in range(len(primeList)): # Find the beginning.
-        if primeList[prime] >= m1:
+def output(): 
+    m, n = [int(i) for i in input().split()] # Reads the segment/range.
+    lp = isPrime(n+1) # Fill the list of primes up to √n.
+    if m == 1:
+        m += 1
+    for beginning in range(len(lp)): # Find the beginning.
+        if lp[beginning] >= m:
             break
-    for prime in range(prime, len(primeList)): # Print the primes between m and n.
-        print(primeList[prime])
+    for prime in range(beginning, len(lp)): # Print the primes between m and n.
+        print(lp[prime])
 
-# ------- Main.
-global primeList
-t = int(input()) # Number of testes.
-for x in range(t): # Performs 't' tests.
-    primeList = [] # Create/Reset the list of primes.
-    m, n = [int(k) for k in input().split()] # Reads the range.
-    output(m,n)
+def test(t): # Define test.
+    for x in range(t): # Performs 't' tests.
+        output()
+
+test(int(input())) # Number of tests.
