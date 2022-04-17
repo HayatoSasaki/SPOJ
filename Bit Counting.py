33819fbc1f1a519be9c66e7b4082b47e58b3fe11
Gabe <99471382+HayatoSasaki@users.noreply.github.com>
2022-04-17 06:59:07 -0300

    Create Bit Counting.py

---
+++ Bit Counting.py
@@ -0,0 +1,4 @@
+# Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary representation of that number. 
+
+def count_bits(n):
+    return str(bin(n)).count('1')
