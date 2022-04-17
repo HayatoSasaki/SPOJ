a92935999858eac3caef98d6c23d1f6d711753c6
Gabe <99471382+HayatoSasaki@users.noreply.github.com>
2022-04-17 07:01:32 -0300

    Update and rename Bit Counting.py to Codewars/6kyu/Bit Counting.py

--- Codewars/6kyu/Bit Counting.py
+++ Codewars/6kyu/Bit Counting.py
@@ -1,4 +1,4 @@
 # Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary representation of that number. 
 
 def count_bits(n):
-    return str(bin(n)).count('1')
+    return str(bin(n)).count('1') # bin(n).count("1") would have worked.
