9bf79466caa8a01b8ed7074ad42a21c98cf4a19d
Gabe <99471382+HayatoSasaki@users.noreply.github.com>
2022-04-20 08:47:09 -0300

    Create Lambda.py

---
+++ Lambda.py
@@ -0,0 +1,8 @@
+# #- Simple Example.
+price = 1000
+tax = lambda x: x*0.3
+print(tax(price)) # 300.
+
+# #- List Example.
+prices = [100, 500, 10, 25]
+print(list(map(lambda x: x*0.3, prices))) # [30.0, 150.0, 3.0, 7.5]
