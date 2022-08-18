f585bb4a55dfc58241257555ec083b65dd398a79
Gabe <99471382+soaresgabe@users.noreply.github.com>
2022-08-18 00:43:42 -0300

    Delete Nesting Structure Comparison.py

--- Coding Challenges/Codewars/4kyu/Nesting Structure Comparison.py
+++
@@ -1,5 +0,0 @@
-# Complete the function/method (depending on the language) to return true/True when its argument is an array 
-# that has the same nesting structures and same corresponding length of nested arrays as the first array.
-
-def same_structure_as(original,other):     
-    pass
